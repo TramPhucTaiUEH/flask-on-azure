77f5fb8b11e5aae8f4376570fa80cb35218757e9
TramPhucTaiUEH <taitram.31221020304@st.ueh.edu.vn>
2025-05-13 16:45:32 +0700

    Fix Flask bind host and port for Render

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Azure!"
+    return "Hello from Render!"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))  # PORT do Render cấp
+    app.run(host="0.0.0.0", port=port)
